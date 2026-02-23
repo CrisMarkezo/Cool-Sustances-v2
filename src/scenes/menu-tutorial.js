9a3e0fea0a54de359f0308c3e623a29dab2b4e43
Laura Ayuso <lauray01@ucm.es>
2026-02-23 19:05:31 +0100

    organización y comienzo del menu de escenas en el tutorial

---
+++ src/scenes/menu-tutorial.js
@@ -0,0 +1,13 @@
+import Phaser from 'phaser'
+import Base from '../base.js';
+
+export default class MenuTutorial extends Phaser.Scene {
+
+    constructor(){
+        super({key: 'phone'});
+    }
+
+    create(){
+        
+    }
+}
