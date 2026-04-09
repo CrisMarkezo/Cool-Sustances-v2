3b52abce20ba293941f1c55f522f418936674730
Laura Ayuso <lauray01@ucm.es>
2026-04-09 13:06:59 +0200

    merge hecho ahora hace falta juntar para que level se pongo al empear y cuando termine el tutorial salte a la escena del tutorial del dia

--- src/scenes/boot.js
+++ src/scenes/boot.js
@@ -144,6 +144,61 @@ super({ key: 'boot' });
     this.load.image('dialogoIcon3', dialogoIcon3);
 
     this.load.image('tiendaIcon', tiendaIcon);
+
+
+    this.load.image('Muebles4', TopDownHouse_FurnitureState1);
+    this.load.image('Muebles6', TopDownHouse_DoorsAndWindows);
+    this.load.image('Paredes y suelos', TopDownHouse_FloorsAndWalls);
+    this.load.image('Objetos pequeños', TopDownHouse_SmallItems);
+
+    this.load.tilemapTiledJSON('tutorial', tutorial);
+    this.load.image('Inventory', Inventory);
+    this.load.image('monster', MonsterImg);
+    this.load.image('healthbar', healthbar);
+
+    // Idle
+    this.load.spritesheet('cat_idle', CatIdle, {
+    frameWidth: 32,
+    frameHeight: 32
+    });
+
+
+    // Run
+    this.load.spritesheet('cat_run', CatRun, {
+    frameWidth: 32,
+    frameHeight: 32
+    });
+
+
+    //Attack gato
+    this.load.spritesheet('cat_attack', CatAttack, {
+        frameWidth: 32,
+        frameHeight: 32
+    });
+
+    this.load.spritesheet('rascador', Rascador, {
+        frameWidth: 32,
+        frameHeight: 32
+    });
+
+
+    this.load.spritesheet('phone', Phone, {
+    frameWidth: 32,
+    frameHeight: 32
+    });
+
+
+
+
+    this.load.spritesheet('cat_grabbing', CatGrabbing, {
+        frameWidth: 32,
+        frameHeight: 32
+    });
+
+
+
+
+
   }
 
   /**
@@ -161,6 +216,64 @@ super({ key: 'boot' });
     ]).then(() => {
       this.scene.start('start-menu');
     });
-    
+    // Animación idle
+this.anims.create({
+    key: 'cat_idle',
+    frames: this.anims.generateFrameNumbers('cat_idle', {
+        start: 0,
+        end: 7
+    }),
+    frameRate: 6,
+    repeat: -1
+});
+
+
+// Run (10 frames)
+this.anims.create({
+    key: 'cat_run',
+    frames: this.anims.generateFrameNumbers('cat_run', {
+        start: 0,
+        end: 9
+    }),
+    frameRate: 12,
+    repeat: -1
+});
+
+
+this.anims.create({
+    key: 'cat_attack',
+    frames: [
+        { key: 'cat_attack', frame: 0 },
+        { key: 'cat_attack', frame: 1 },
+
+        
+        { key: 'cat_attack', frame: 2 },
+        { key: 'cat_attack', frame: 2 },
+        { key: 'cat_attack', frame: 2 },
+
+        { key: 'cat_attack', frame: 3 },
+        { key: 'cat_attack', frame: 4 },
+        { key: 'cat_attack', frame: 5 }
+    ],
+    frameRate: 14,
+    repeat: 0
+});
+
+this.anims.create({
+    key: 'rascador_scratch',
+    frames: this.anims.generateFrameNumbers('rascador', { start: 1, end: 4 }), // frames que loopearán
+    frameRate: 6,
+    repeat: 0 // loop infinito hasta que la pares
+});
+
+
+this.anims.create({
+    key: 'cat_grabbing',
+    frames: this.anims.generateFrameNumbers('cat_grabbing', { start: 0, end: 2 }),
+    frameRate: 6,   // 6 fps, ajusta para que dure como quieras
+    repeat: 0        // se reproduce una vez
+});
+
+this.scene.start('level');
   }
 }
