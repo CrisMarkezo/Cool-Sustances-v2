735e0e30db33ff7fe27693b417ef900238c886a7
CrisMarkezo <161339256+CrisMarkezo@users.noreply.github.com>
2026-04-22 08:07:26 +0200

    Nuevos assets para la dungeon

---
+++ assets/dungeon/decoraciones.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="decoraciones" tilewidth="16" tileheight="16" tilecount="210" columns="21">
+ <image source="decorations.png" width="336" height="160"/>
+</tileset>
