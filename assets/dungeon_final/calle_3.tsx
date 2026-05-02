5667a498a6bef6be948e196c3ef4fad056efca45
Guille <100872460+Trygach@users.noreply.github.com>
2026-05-02 14:38:04 +0200

    Add files via upload

---
+++ assets/dungeon_final/calle_3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="calle_3" tilewidth="16" tileheight="16" tilecount="4" columns="1">
+ <image source="assets/Simple beat-em-up assets/Assets/props/lamp_down.png" width="16" height="64"/>
+</tileset>
